5c5205ad25352fbbf6614db2918678b8fd31d7de
João Paulo <joaopaulo.tfa@gmail.com>
2022-03-18 00:24:26 -0300

    creating form components

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,22 +1,27 @@
-function ProjectForm() {
-    return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total"/>
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled>Selecione a categoria</option> 
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar projeto" />
-            </div>
-        </form>
-    )
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+import styles from "./ProjectForm.module.css";
+
+function ProjectForm({btnText}) {
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do Projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento do Projeto"
+        name="budget"
+        placeholder="Insira o orçamento total"
+      />
+      <Select name="category_id" text="Selecione a categoria" />
+      <SubmitButton text={btnText} />
+    </form>
+  );
 }
 
-export default ProjectForm
+export default ProjectForm;
